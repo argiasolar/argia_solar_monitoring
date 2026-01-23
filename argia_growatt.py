b3eb09f704fab89995eea8eacdccacdff595abe3
Zemel <tzemelka@gmail.com>
2026-01-23 09:45:29 -0600

    Update argia_growatt.py

--- argia_growatt.py
+++ argia_growatt.py
@@ -4,8 +4,8 @@ def fetch_growatt_data(target_date, plant_keys):
     """Pobiera dane produkcyjne z Growatt Server."""
     print(f"🚀 [Growatt] Starting data import for {target_date}...")
     
-    user = os.environ.get('GROWATT_USER')
-    password = os.environ.get('GROWATT_PASS')
+    user = os.environ.get('GROWATT_USERNAME')
+    password = os.environ.get('GROWATT_PASSWORD')
     
     if not user or not password:
         print("⚠️  [Growatt] Missing credentials. Skipping Growatt.")
